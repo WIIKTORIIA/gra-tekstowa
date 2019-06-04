ea793609bd764a382e0b0bfec8c378a5a2b99a1a
gg <>
2019-06-04 09:07:57 +0200

    dodano cały projekt z unity

---
+++ płytka gra/Assets/bylerjak.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bylerjak : MonoBehaviour
+{
+    public GameObject[] okna;
+    public int liczbaokien;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < okna.Length; i++)
+        {
+            if (i == liczbaokien)
+            {
+                okna[i].gameObject.active = true;
+            }
+            else
+            {
+                okna[i].gameObject.active = false;
+            }
+        }
+    }
+
+    public void Przycisk(int lp)
+    {
+        liczbaokien = lp;
+        if (lp == 6)
+            SceneManager.LoadScene(0);
+    }
+}
---
+++ płytka gra/Assets/menusc.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menusc : MonoBehaviour
+{
+
+    public void Graj()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Wyjscie()
+    {
+        Application.Quit();
+    }
+}
